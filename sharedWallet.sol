19db926291f9530f34fb83aa6195ce26cad60a3b
Oyinda David <oyindadavid18@gmail.com>
2021-07-16 19:52:53 +0100

    give max amount to withdrawal

--- sharedWallet.sol
+++ sharedWallet.sol
@@ -3,18 +3,20 @@ pragma solidity >=0.7.0 <0.9.0;
 contract SharedWallet {
     // amount in the wallet
     uint public balance;
+    uint public maxWithdrawalAmount = 100;
     
     struct Account {
         address payable account;
         bool permitted;
+        uint amountToWithdraw;
     }
     
     Account[] public permittedAccounts;
     
-    function getAccountIndex(address _addr) public view returns (uint) {
-    for(uint i = 0; i< permittedAccounts.length; i++){
-      if(_addr == permittedAccounts[i].account) {
-             return i;
+    function getAccountIndex(address _addr) public view returns (uint i) {
+    for(uint _i = 0; _i< permittedAccounts.length; _i++){
+      if(_addr == permittedAccounts[_i].account) {
+             return _i;
       }
    
     }
@@ -26,18 +28,27 @@ contract SharedWallet {
         _;
     }
     
+    modifier onlyWithdrawalAmount(address _addr) {
+        uint accountIndex = getAccountIndex(_addr);
+        require(permittedAccounts[accountIndex].amountToWithdraw >= maxWithdrawalAmount);
+        _;
+    }
+    
     function giveAccountPermission(Account memory _account) public {
         require(_account.permitted == true, "You have already been granted permission to use this account");
         _account.permitted = true;
         permittedAccounts.push(Account({
             account: _account.account,
-            permitted: _account.permitted
+            permitted: _account.permitted,
+            amountToWithdraw: _account.amountToWithdraw
         }));
     }
     
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyPermittedAccount(_to)  {
+    function withdrawMoney(address payable _to, uint _amount) public onlyPermittedAccount(_to) onlyWithdrawalAmount(_to) {
         _to.transfer(_amount);
+        
+        balance -= _amount;
     }
     
     function deposit(uint _amount) public  {
